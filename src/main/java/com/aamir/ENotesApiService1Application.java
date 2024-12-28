bedc45f8fa875ee5b4602a535661c1373680b05d
AamirRuhi <t4aamir@gmail.com>
2024-12-28 12:44:05 +0530

    initial commit

---
+++ src/main/java/com/aamir/ENotesApiService1Application.java
@@ -0,0 +1,13 @@
+package com.aamir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ENotesApiService1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ENotesApiService1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/aamir/ENotesApiService1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aamir;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ENotesApiService1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
