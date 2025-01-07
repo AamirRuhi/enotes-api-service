01d918d6b0d5114dac35a290d67a8a7c4d4e711d
AamirRuhi <t4aamir@gmail.com>
2025-01-07 16:03:18 +0530

    todo-module-pendind

--- src/main/java/com/aamir/ENotesApiService1Application.java
+++ src/main/java/com/aamir/ENotesApiService1Application.java
@@ -16,13 +16,24 @@ public class ENotesApiService1Application {
 
 	
 	/* {
-    "title":"python with boot",
-    "description":"python is programming language",
-    "category":{
-        "id":58
+	 * for save todo
+    {
+    "title":"java core",
+    "status":{
+        "id":1
     }
+}http://localhost:8081/api/v1/todo/1 id ke sath naam nhi aa rha hai naam lane ke liye
+//in progress me hai to emil send krenge plz complete it 12pm pe automatic TO USER CHAIYE TO USER MODULE KRLO
 }  
-copy notes row copy krne ke liye
+todo management ---> set task status Todo entity bnao
+status ko manage krne ke liye enums pakage me TodoEnums class bnayege
+definition : Enums are used for representing a fixed set of related constants in a structured and meaningful way. 
+1->Status Codes (e.g., ACTIVE, INACTIVE, PENDING)
+2->Days of the Week (e.g., MONDAY, TUESDAY)
+3->Directions (e.g., NORTH, SOUTH, EAST, WEST)
+4->Payment Methods (e.g., CREDIT_CARD, PAYPAL, CASH)
+5->Order States (e.g., PLACED, SHIPPED, DELIVERED, CANCELLED)
+
 */ 
 
 }
